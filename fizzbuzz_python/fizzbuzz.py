ceff7893cae80221e91090b758d6c931fc905b5f
Walker-TW <thomas.w.walker@live.co.uk>
2020-04-15 21:42:51 +0100

    Added better fizzbuzz

--- fizzbuzz_python/fizzbuzz.py
+++ fizzbuzz_python/fizzbuzz.py
@@ -1,4 +1,21 @@
-def Fizzbuzz(x):
+def better_fizzbuzz (x):
+  for x in range(x):
+
+    output = ""
+
+    if ( x % 3 == 0):
+      output += "Fizz"
+    if ( x % 5 == 0):
+      output += "Buzz"
+    
+    if output == "":
+      print ( x )
+    else:
+      print ( output )
+
+# OR
+
+def simple_fizzbuzz(x):
   if x % 3 == 0:
     print ("Fizz")
   elif x % 5 == 0:
@@ -9,11 +26,11 @@ def Fizzbuzz(x):
     print (x)
 
 for x in range(101):
-  Fizzbuzz(x)
+  simple_fizzbuzz(x)
 
 # OR 
 
-# i = 0
-# while i < 100:
-#   i += 1
-#   Fizzbuzz(i)
+i = 0
+while i < 100:
+  i += 1
+  simple_fizzbuzz(i)
